184d18e5c3da83887e967af13a100b36fba6a4bf
sajjad-vrg <sajjad@vrguest.com>
2019-02-25 18:09:33 +0500

    fixing config for heroku

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  client_id: 'i7rgaquwvcg9e9a5scx2ymkttlrzs4',
-  client_secret: 'qes8dxe7wpzha34hxi3p7sbi7xb7up',
-  redirect_uri: 'http://localhost:4000/authorize',
+  client_id: process.env.CLIENT_ID || 'i7rgaquwvcg9e9a5scx2ymkttlrzs4',
+  client_secret: process.env.CLIENT_SECRET || 'qes8dxe7wpzha34hxi3p7sbi7xb7up',
+  redirect_uri: process.env.REDIRECT_URI || 'http://localhost:4000/authorize',
   grant_type: 'authorization_code',
   secret: 'somerandomstringsecretshouldgohere'
 };
