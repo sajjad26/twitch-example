4ae140b205f610afca6d62c9b9f8ef328c9a9034
sajjad26 <mohdsajjadashraf@yahoo.com>
2019-03-02 12:49:55 +0500

    adding webhooks

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const config = {
   client_id: process.env.CLIENT_ID || 'i7rgaquwvcg9e9a5scx2ymkttlrzs4',
   client_secret: process.env.CLIENT_SECRET || 'qes8dxe7wpzha34hxi3p7sbi7xb7up',
   base_url: baseUrl,
-  redirect_uri: `${process.env.REDIRECT_URI || baseUrl}/authorize`,
+  redirect_uri: `${baseUrl}/authorize`,
   grant_type: 'authorization_code',
   secret: 'somerandomstringsecretshouldgohere'
 };
