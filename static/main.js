d5d9039a0e2ef5c226c63d6197884d2977769def
sajjad26 <mohdsajjadashraf@yahoo.com>
2019-03-04 21:44:17 +0500

    fixing add streamer page

--- static/main.js
+++ static/main.js
@@ -2,7 +2,10 @@ $(document).ready(function () {
   $("#addStreamerForm").submit(function (e) {
     e.preventDefault();
     var streamerId = $(this).find('input[name="streamer"]').val();
+    var $button = $(this).find('#submitBtn');
+    var buttonText = $button.text();
     if(streamerId){
+      $button.text('Adding...').prop('disabled', true);
       $.post('/add-streamer', {
         streamer: streamerId
       }).then(function(res){
@@ -10,12 +13,14 @@ $(document).ready(function () {
           throw new Error(res.error);
         }else{
           $('.streamer-error').remove();
+          $button.text(buttonText).prop('disabled', false);
           window.location.href = '/stream';
         }
       }).catch(function(err) {
         $('.streamer-error').remove();
         const errorElement = "<div class='streamer-error text-danger'>" + err.toString() + "</div>";
         $(errorElement).insertAfter('#streamerHelp');
+        $button.text(buttonText).prop('disabled', false);
       });
     }
   });
